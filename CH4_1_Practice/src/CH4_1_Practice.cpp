0e571cae500eb07407e056c3bc20fe4b3e3edca2
hoeyoon <hoeiyoon57@gmail.com>
2026-03-21 00:05:24 +0900

    CH4_1_Practice Start

---
+++ CH4_1_Practice/src/CH4_1_Practice.cpp
@@ -0,0 +1,111 @@
+/******************************************************************************
+ * 코드 추가 1
+ ******************************************************************************/
+// 1) 메뉴 [File]->[New]->[C/C++ Project] 한 후 새로운 프로젝트 CH4을 만들어라.
+// 2) 메뉴 [File]->[New]->[Source Folder] 한 후 폴더 이름으로 src를 지정하여 생성하라.
+// 3) 메뉴 [File]->[New]->[Source File]   한 후 폴더 이름으로는 위에서 지정한 CH4/src로
+//    설정하고 파일 이름으로 ch4.cpp를 지정하여 소스파일을 만들어라.
+// 4) 아래 코드를 소스파일로 복사하라.
+//-----------------------------------------------------------------------------
+
+// iostream string head 파일 include할 것
+#include <iostream>
+#include <string>
+using namespace std;
+
+/******************************************************************************
+ * class Container
+ ******************************************************************************/
+
+class Container {
+    string title;  // 컨테이너 타이틀
+    int    size;   // 컨테이너에 저장된 정수 배열 arr의 크기
+    int    *arr;   // size 개의 원소를 가진 배열 시작 주소
+public:
+    void setTitle(string title);
+    string getTitle();
+    void newArray();
+    void average();
+    void rightRotateTitle();
+    void findDeleteTitle();
+};
+
+
+void Container::setTitle(string title) {
+    /* 매개변수 title 값을 멤버 변수 title에 저장한다. */
+}
+
+string Container::getTitle() {
+    return "";  /* 멤버변수 title을 반환하라. */
+}
+
+/******************************************************************************
+ * Global functions
+ ******************************************************************************/
+
+void constructor() {
+    Container c;
+    c.setTitle("constructor-title");
+    cout << "c.getTitle(): " << c.getTitle() << endl;
+}
+
+void intArray() {
+}
+
+Container *newObjectArray(int size) {
+    Container *pArr = nullptr; // Container 객체 배열 시작 주소
+    /* 여기에 코드를 구현하라. */
+    return pArr;
+}
+
+void printTitles(Container *pArr, int size) {
+}
+
+void objectArray() {
+    Container *pArr; // Container 객체 배열 시작 주소
+    int size;        // Container 객체 배열 원소 개수
+    cout << "element numbers of Container array[]? ";
+    cin >> size;
+    pArr = newObjectArray(size);
+    printTitles(pArr, size);
+    /* 여기서 pArr이 포인터 하는 메모리를 반납하시오. */
+}
+
+void inputTitle(Container* cp) {
+    string s;
+    getline(cin, s); // 앞서 입력된 메뉴항목 번호 뒤의 [엔터]를 제거함
+    /* 여기에 코드를 구현하라. */
+}
+
+void rotateTitle() {
+}
+
+void findDelete() {
+}
+
+/******************************************************************************
+ * main()
+ ******************************************************************************/
+
+string menuStr =
+"******************************** Main Menu *********************************\n"
+"* 0.Exit 1.constructor 2.intArray 3.objectArray 4.rotateTitle 5.findDelete *\n"
+"****************************************************************************\n";
+
+int main() {
+    while (true) {
+        int menuItem;
+        cout << endl << menuStr << "menu? ";
+        cin >> menuItem;
+        if (menuItem == 0)
+            break;
+        switch(menuItem) {
+        case 1: constructor(); break;
+        case 2: intArray();    break;
+        case 3: objectArray(); break;
+        case 4: rotateTitle(); break;
+        case 5: findDelete(); break;
+        }
+    }
+    cout << "Good bye!!" << endl;
+}

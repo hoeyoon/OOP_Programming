81c3c281f35f8dbf4058e1fc358560bfab7fa44e
hoeyoon <hoeiyoon57@gmail.com>
2026-04-06 20:37:58 +0900

    CH6_1_liveCoding Finish

--- CH6_1_liveCoding/src/CH6_1_liveCoding.cpp
+++ CH6_1_liveCoding/src/CH6_1_liveCoding.cpp
@@ -7,39 +7,86 @@ using namespace std;
 class Player {
 private:
     //멤버 변수
+	string name;
+	int health;
+	int weapon;
+
 public:
    //멤버 함수
+	Player(){}
+	Player(string name, int health);
+	Player(string name, int health, int weapon);
+	~Player();
+
+	void getInfo();
+	string getName(){ return name; }
+	int getHealth(){ return health; }
+	int getWeapon(){ return weapon; }
+	void setHealth(int health){ this->health = health; }
+	void setWeapon(int weapon){ this->weapon = weapon; }
 };
+
+Player::Player(string name, int health) : Player(name, health, 10){}
+
+Player::Player(string name, int health, int weapon) : name(name), health(health), weapon(weapon){}
+
+Player::~Player(){}
 
 void Player::getInfo() {
     cout << "-----플레이어 정보-----" << endl;
-    cout << "플레이어 이름 : " << << endl;
-    cout << "체력 : " << << endl;
-    cout << "무기 : " <<  << endl;
+    cout << "플레이어 이름 : " << name << endl;
+    cout << "체력 : " << health << endl;
+    cout << "무기 : " << weapon << endl;
 }
 
 
 class Monster {
 private:
     //멤버 변수
+	string name;
+	int health;
+	int weapon;
+
 public:
    //멤버 함수
+	Monster();
+	Monster(string name);
+	Monster(string name, int health);
+	~Monster();
+
+	void getInfo();
+	string getName(){ return name; }
+	int getHealth(){ return health; }
+	int getWeapon(){ return weapon; }
+	void setHealth(int health){ this->health = health; }
+	void setWeapon(int weapon){ this->weapon = weapon; }
 };
+
+Monster::Monster(string name) : Monster(name, 100){}
+
+Monster::Monster(string name, int health)
+: name(name), health(health), weapon(1){}
+
+Monster::~Monster(){}
+
 void Monster::getInfo() {
     cout << "-----몬스터 정보-----" << endl;
-    cout << "몬스터 이름 : " <<  << endl;
-    cout << "체력 : " <<  << endl;
-    cout << "무기 : " <<  << endl;
+    cout << "몬스터 이름 : " << name << endl;
+    cout << "체력 : " << health << endl;
+    cout << "무기 : " << weapon << endl;
 }
 
-void Attack() {
-  cout << << "(이)가 " <<  << "을 공격했다!" << endl;
+void Attack(Player &p, Monster& m) {
+	cout << p.getName() << "(이)가 " << m.getName() << "을 공격했다!" << endl;
+	m.setHealth(m.getHealth() - p.getWeapon());
 }
-void Attack() {
-  cout << << "(이)가 " <<  << "을 공격했다!" << endl;
+void Attack(Monster& m, Player& p) {
+	cout << m.getName() << "(이)가 " << p.getName() << "을 공격했다!" << endl;
+	p.setHealth(p.getHealth() - m.getWeapon());
 }
-void Attack() {
-  cout << << "(이)가 " <<  << "을 공격했다!" << endl;
+void Attack(Player& p1, Player& p2) {
+	cout << p1.getName() << "(이)가 " << p2.getName() << "을 공격했다!" << endl;
+	p2.setHealth(p2.getHealth() - p1.getWeapon());
 }
 
 int main() {

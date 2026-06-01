99eb21f14addfb4f85432ce53aaa48ea43a26185
hoeyoon <hoeiyoon57@gmail.com>
2026-06-01 22:30:31 +0900

    CH10_1_liveCoding Start

---
+++ CH10_1_liveCoding/src/CH10_1_liveCoding.cpp
@@ -0,0 +1,27 @@
+#include <iostream>//공백 지우고 활용
+#define MAX_ARRAY_SIZE 6
+using namespace std;
+
+template
+ 	 smallest( a[], int n) {
+
+}
+
+int main() {
+	int x[MAX_ARRAY_SIZE];
+	double y[MAX_ARRAY_SIZE];
+
+	cout << "정수 " << MAX_ARRAY_SIZE << "개를 입력하세요:";
+	for (int i = 0; i < MAX_ARRAY_SIZE; i++)
+		cin >> x[i];
+	cout << "가장 작은 정수는: " << smallest(x, MAX_ARRAY_SIZE) << endl;
+
+	
+	cout << "실수 " << MAX_ARRAY_SIZE << "개를 입력하세요:";
+	
+	for (int i = 0; i < MAX_ARRAY_SIZE; i++)
+		cin >> y[i];
+	cout << "가장 작은 실수는: " << smallest(y, MAX_ARRAY_SIZE) << endl;
+
+	return 0;
+}

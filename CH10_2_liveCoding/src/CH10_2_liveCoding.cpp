6d41183db76409e05106ecc8521a3ce874a36bef
hoeyoon <hoeiyoon57@gmail.com>
2026-06-01 22:41:59 +0900

    CH10_2_liveCoding Finish

--- CH10_2_liveCoding/src/CH10_2_liveCoding.cpp
+++ CH10_2_liveCoding/src/CH10_2_liveCoding.cpp
@@ -1,13 +1,13 @@
 #include <iostream> //공백지우고 활용
 #include <vector> //공백지우고 활용
 #include <algorithm> //공백지우고 활용 
-#define MAX_SIZE 5
+#define MAX_SIZE  5
 using namespace std;
 
 
 int main() {
 
-     strVec;
+    vector<string> strVec;
 
     // 문자열 입력 받기
     cout << "문자열을 최대 " << MAX_SIZE << "개 입력하세요:" << endl;
@@ -16,10 +16,12 @@ int main() {
         cout << "문자열 " << i + 1 << ": ";
         cin >> str;
         //vector에 삽입하는 코드 필요
+        strVec.push_back(str);
     }
 
     cout << "각 문자열의 길이:" << endl;
-    for_each( , ,[  ](  ){  });
+    for_each(strVec.begin(), strVec.end(), [  ](const string& str){ cout << str << "의 길이: " << str.length() << endl; });
     // string 자료형의 문자열 길이를 구하는 함수는 .length() 이다.
     return 0;
 }
+mbti에 대해 설명하는 문서를 만들어줘
